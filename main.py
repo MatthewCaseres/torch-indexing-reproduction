7746451386c902e57d75cd08ba8af0b3147964c6
Matthew Caseres <matthewcaseres@outlook.com>
2024-04-08 00:02:11 +0000

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import logging
+
+class CompiledClass(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.nums = torch.tensor([1,2,3,4,5,6,7,8,9,10])
+        self.t = 5
+    
+    def forward(self):
+        self.num = self.nums[self.t//12]
+        self.t += 1
+        return self.num
+    
+m = CompiledClass()
+m = torch.compile(m)
+
+torch._logging.set_logs(dynamo = logging.DEBUG)
+torch._dynamo.config.verbose = True
+
+# the first call works
+m()
+# the second call causes a failure
+m()
